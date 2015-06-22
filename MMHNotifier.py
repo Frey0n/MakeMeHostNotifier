652a06ac049eb0c0f6015e7b63806ec8d2135ee6
Francesco Marazzi <framar90@hotmail.it>
2015-06-22 22:22:36 +0200

    Updating

--- MMHNotifier.py
+++ MMHNotifier.py
@@ -4,6 +4,7 @@ import notify
 import datetime
 import sys
 
+
 def GameName(f,name):
     cent=f.lower().index(name)
     while f[cent] != '>':
@@ -15,18 +16,23 @@ def GameName(f,name):
     p_bot=x=top+9
     while f[x] != '<':
         x+=1
-    p_top=x    
-    print  'Game name: ',f[bot:top],' ',f[p_bot:p_top]
+    p_top=x
+    gname=f[bot:top]
+    gplay=f[p_bot:p_top]
+    print 'Game name: '+gname+' '+gplay
+    return gname,gplay 
+
 
 def GameSearch(f,tests):
     for name in tests:
         if name in f.lower():
-            print 'up!', datetime.datetime.now().strftime("%H:%M")
-            GameName(f,name)
-            notify.balloon_tip('Game up!', 'It is time!')
+            print 'up!', datetime.datetime.now().strftime("%H:%M"),
+            gname,gplay = GameName(f,name)
+            notify.balloon_tip('Game up!', 'Game name: '+gname+' '+gplay)
             return 1
     print 'down', datetime.datetime.now().strftime("%H:%M")
     return 0
+    
 try:
     a=float(sys.argv[1])
 except:
